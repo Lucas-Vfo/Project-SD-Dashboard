ae1edd8599af19fc86fa44afe615503d1ab68950
Lucas-Vfo <72819147+Lucas-Vfo@users.noreply.github.com>
2023-12-31 16:31:42 -0300

    Conextion BD - Category & Products

---
+++ prisma/migrations/20231231033417_second/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PasswordRecovery" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiration" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PasswordRecovery_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL DEFAULT 999,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "categoryId" INTEGER NOT NULL,
+    "stock" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/routes/categories.routes.js
@@ -0,0 +1,59 @@
+import { Router } from "express";
+import { prisma } from '../db.js';
+
+
+const router = Router();
+
+router.get('/categories', async (req, res) => {
+    const categories = await prisma.category.findMany()
+    res.json(categories);
+});
+
+router.get('/categories/:id', async (req,res) => {
+    const categoryFound = await prisma.category.findFirst({
+        where: {
+            id: parseInt(req.params.id),
+        },
+        include: {
+            products: true,
+        }
+    })
+
+    if (!categoryFound)
+        return res.status(404).json({ error: "Category not found" });
+
+    return res.json(categoryFound);
+});
+
+router.post('/categories', async (req, res) => {
+    const newCategory = await prisma.category.create({
+        data: req.body,
+    })
+    res.json(newCategory);
+});
+
+router.delete('/categories/:id', async (req,res) => {
+    const categoryDeleted = await prisma.category.delete({
+        where: {
+            id: parseInt(req.params.id),
+        },
+    })
+
+    if (!categoryDeleted)
+        return res.status(404).json({ error: "Category not found" });
+    
+    return res.json(categoryDeleted);
+});
+
+router.put('/categories/:id', async (req,res) => {
+    const categoryUpdate = await prisma.category.update({
+        where: {
+            id: parseInt(req.params.id),
+        },
+        data: req.body
+    })
+    
+    return res.json(categoryUpdate);
+});
+
+export default router;
